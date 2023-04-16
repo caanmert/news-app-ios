fd13efb1a0922de73b17e55fdb541c031a1af0e3
canmert <canmert.dev@gmail.com>
2023-04-16 19:29:11 +0300

    feat: add Custom Button to reuse between views

--- news-app/components/CustomButton.swift
+++ news-app/components/CustomButton.swift
@@ -1,8 +1,26 @@
-//
-//  CustomButton.swift
-//  news-app
-//
-//  Created by jaerka on 16.04.23.
-//
 
 import Foundation
+import SwiftUI
+
+
+struct CustomButton: View{
+    var text:String
+    var action: () -> Void
+    
+    var body: some View{
+        Button(action:action){
+        Text(text)
+            .foregroundColor(.white)
+            .frame(maxWidth: .infinity, maxHeight: 75)
+            .cornerRadius(30)
+            .shadow(radius: 40)
+            
+        }
+        .background(Color.blue)
+        .padding(.top)
+        .padding(.horizontal)
+    
+        
+    }
+    
+}
